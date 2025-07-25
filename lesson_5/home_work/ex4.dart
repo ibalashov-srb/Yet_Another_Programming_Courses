05a9ae54e3735420c88801b450b06347d5c8f4ef
Ivan Balashov <ivan.balashov@softswiss.com>
2025-07-25 21:21:51 +0200

    new lesson

--- lesson_5/home_work/ex4.dart
+++ lesson_5/home_work/ex4.dart
@@ -8,8 +8,8 @@
 
 void main() {
   Map<String, int> prices = {'яблоко': 30, 'хлеб': 40, 'молоко': 50};
+  
   Map<String, int> cart = {'яблоко': 2, 'хлеб': 1};
-
   print(getTotalPrice(cart, prices) == 100 ? 'Test 1 Passed' : 'Test 1 Failed');
 
   Map<String, int> cart2 = {'молоко': 3, 'яблоко': 1};
@@ -20,10 +20,7 @@ void main() {
 
 int getTotalPrice(Map<String, int> cart, Map<String, int> prices) {
   var totalPrice = 0;
-
-  print(prices);
   var pricesWithDiscount = getDiscount(prices, 0.25);
-  print(pricesWithDiscount);
 
   for (var entry in cart.entries) {
     var price = pricesWithDiscount[entry.key]! * cart[entry.key]!;
@@ -35,14 +32,11 @@ int getTotalPrice(Map<String, int> cart, Map<String, int> prices) {
 
 int getZdachya(Map<String, int> cart, Map<String, int> prices, int money) {
   var total = getTotalPrice(cart, prices);
-
   var zdachya = money - total;
+
   return zdachya;
 }
 
-/**
- * discountPercentage = 25% => 0.25
- */
 Map<String, int> getDiscount(Map<String, int> prices, double discountPercentage) {
   Map<String, int> pricesWithDiscount = {};
 
