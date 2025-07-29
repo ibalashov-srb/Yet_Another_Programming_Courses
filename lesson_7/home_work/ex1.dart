f0bafc04844d37bd7c689096ca386ab635317a66
Ivan Balashov <ivan.balashov@softswiss.com>
2025-07-29 11:04:27 +0200

    hw

---
+++ lesson_7/home_work/ex1.dart
@@ -0,0 +1,17 @@
+/**
+ * Задание 1: Класс Book
+ * Создай класс Book с полями:
+ * title (String)
+ * author (String)
+ * year (int)
+ * Создай 3 экземпляра книг и выведи информацию о них через print.
+ */
+class Book {
+  // TODO: добавь поля title, author, year
+}
+
+void main() {
+  // TODO: создай 3 книги
+
+  // TODO: выведи информацию о каждой книге
+}
