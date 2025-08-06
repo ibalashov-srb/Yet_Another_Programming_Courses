cb4e33a9176044873e48e35abdae651dfd98128f
Ivan Balashov <ivan.balashov@softswiss.com>
2025-08-06 11:16:09 +0200

    hw

---
+++ lesson_10/home_work/ex_4.dart
@@ -0,0 +1,19 @@
+// 📄 main.dart
+// ЗАДАНИЕ 4:
+// 1. utils/math.dart → sum, multiply
+// 2. utils/helpers.dart → greet(String name)
+// 3. utils/exports.dart → экспортирует math.dart и helpers.dart
+// 4. Импортируй exports.dart сюда
+
+// TODO: импортируй 'utils/exports.dart'
+
+void runUtilsTest() {
+  // TODO: раскомментируй после реализации
+  // int s = sum(3, 7);
+  // int m = multiply(4, 5);
+  // String greeting = greet("Ivan"); // пусть greet возвращает строку
+
+  // print(s == 10 ? 'Test 4.1 Passed' : 'Test 4.1 Failed');
+  // print(m == 20 ? 'Test 4.2 Passed' : 'Test 4.2 Failed');
+  // print(greeting == "Привет, Ivan" ? 'Test 4.3 Passed' : 'Test 4.3 Failed');
+}
